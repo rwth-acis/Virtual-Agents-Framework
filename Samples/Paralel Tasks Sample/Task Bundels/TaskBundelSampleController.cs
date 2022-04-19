82c6272236034495549c3f324407f0c6818f2240
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2022-04-19 22:21:02 +0200

    Task bundles and animation tasks can now be used through the shortcuts
    Signed-off-by: Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>

--- Samples/Paralel Tasks Sample/Task Bundels/TaskBundelSampleController.cs
+++ Samples/Paralel Tasks Sample/Task Bundels/TaskBundelSampleController.cs
@@ -25,8 +25,10 @@ namespace i5.VirtualAgents.Examples
             IAgentTask wait = new AgentWaitTask(2);
             agent.ScheduleTask(wait, 0, "Left Arm");
 
+            //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
             //These tasks will be configurated to start and end simultaniously.
-            //Therefore, shaking the head will not start immediately but it will only start when the waving starts which was delayed by 2 seconds by the waiting task--------------------
+            //Therefore, shaking the head will not start immediately but it will only start when the waving starts which was delayed by 2 seconds by the waiting task.
+            //The manual way to schedule a bundle.
 
             TaskSynchroniser synchroniserStart = new TaskSynchroniser();
             TaskSynchroniser synchroniserEnd = new TaskSynchroniser();
@@ -36,20 +38,33 @@ namespace i5.VirtualAgents.Examples
             animationTask.ReadyToStart.Add(synchroniserStart.WaitForOtherTasksMutually(animationTask));
             animationTask.ReadyToEnd = new List<System.Func<bool>>();
             animationTask.ReadyToEnd.Add(synchroniserEnd.WaitForOtherTasksMutually(animationTask));
-            agent.ScheduleTask(animationTask,0, "Left Arm");
+            agent.ScheduleTask(animationTask, 0, "Left Arm");
 
             animationTask = new AgentAnimationTask("ShakeHead", 10);
             animationTask.ReadyToStart = new List<System.Func<bool>>();
             animationTask.ReadyToStart.Add(synchroniserStart.WaitForOtherTasksMutually(animationTask));
             animationTask.ReadyToEnd = new List<System.Func<bool>>();
             animationTask.ReadyToEnd.Add(synchroniserEnd.WaitForOtherTasksMutually(animationTask));
-            agent.ScheduleTask(animationTask,0,"Head");
+            agent.ScheduleTask(animationTask, 0, "Head");
+
             //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+            agent.Tasks.WaitForSeconds(1, default, "Head");
+
+            //The same bundle, but scheduled via shortcuts
+            
+            agent.Tasks.OpenNewBundle();
+            agent.Tasks.PlayAnimation("Wave", 5, "", 0, "Left Arm");
+            agent.Tasks.PlayAnimation("ShakeHead", 10, "", 0, "Head");
+            agent.Tasks.CloseBundle();
+
+
+            //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
             //Wave on arm layer. Will only start once the headshaking from the interlocked tasks is finished, since the provious wave task will block the arm lane until all other interlocked tasks are finished------
-            animationTask = new AgentAnimationTask("Wave", 5);
-            agent.ScheduleTask(animationTask, 0, "Left Arm");
+            //animationTask = new AgentAnimationTask("Wave", 5);
+            //agent.ScheduleTask(animationTask, 0, "Left Arm");
             //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
