efa4c46a8b1c6d50996bc0244cc61937308b944b
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2022-04-19 22:21:02 +0200

    Task bundles and animation tasks can now be used through the shortcuts
    Signed-off-by: Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>

--- Assets/Virtual Agents Framework/Runtime/Scripts/TaskSystem/TaskActions.cs
+++ Assets/Virtual Agents Framework/Runtime/Scripts/TaskSystem/TaskActions.cs
@@ -1,4 +1,5 @@
 using i5.VirtualAgents.TaskSystem.AgentTasks;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace i5.VirtualAgents.TaskSystem
@@ -10,6 +11,10 @@ namespace i5.VirtualAgents.TaskSystem
     {
         // the agent for which these tasks actions are
         private Agent agent;
+
+        //Synchroniser for managing task bundles
+        private TaskSynchroniser synchroniserStart;
+        private TaskSynchroniser synchroniserEnd;
 
         /// <summary>
         /// Creates a new task actions shortcut and registers the agent on which the tasks are scheduled
@@ -29,7 +34,7 @@ namespace i5.VirtualAgents.TaskSystem
         public void GoTo(Vector3 destinationCoordinates, int priority = 0)
         {
             AgentMovementTask movementTask = new AgentMovementTask(destinationCoordinates);
-            agent.ScheduleTask(movementTask, priority);
+            ScheduleWithRespectTobundle(movementTask, priority);
         }
 
         /// <summary>
@@ -56,11 +61,86 @@ namespace i5.VirtualAgents.TaskSystem
 
         /// <summary>
         /// Lets the agent wait for the given number of seconds in an idle position
+        /// Shortcut queue management function
         /// </summary>
         /// <param name="seconds">The time span in seconds for which the agent shoudl wait</param>
-        public void WaitForSeconds(float seconds, int priority = 0)
+        /// <param name="layer">The animation layer on which the task should be excuted</param>
+        public void WaitForSeconds(float seconds, int priority = 0, string layer = "Base Layer")
         {
-            agent.ScheduleTask(new AgentWaitTask(seconds), priority);
+            ScheduleWithRespectTobundle(new AgentWaitTask(seconds), priority, layer);
+        }
+
+
+        /// <summary>
+        /// Play an animation through the agents animation controller
+        /// Shortcut queue management function
+        /// </summary>
+        /// <param name="startTrigger"></param> Trigger that starts the animation
+        /// <param name="playTime"></param> Time in seconds after which the animation should stop
+        /// <param name="stopTrigger"></param> Trigger that stops the animation. If not provided, start trigger is used again
+        /// <param name="priortiy"></param>
+        /// <param name="layer"></param> The animation layer on which the task should be excuted
+        public void PlayAnimation(string startTrigger, float playTime, string stopTrigger = "", int priortiy = 0, string layer = "Base Layer")
+        {
+            IAgentTask animationTask = new AgentAnimationTask(startTrigger,playTime,stopTrigger);
+            ScheduleWithRespectTobundle(animationTask,priortiy,layer);
+        }
+
+
+        /// <summary>
+        /// Every task that will be scheduled through the task actions after opening a bundle and before closing it will automatically be added to taht bundle.
+        /// Task that are part of a bundle will wait with starting/ending until all other task from the budle are ready to start/end
+        /// </summary>
+        /// <param name="waitForStart"></param> Should tasks wait with starting for all other tasks from the bundle
+        /// <param name="waitForEnd"></param>Should tasks wait with ending for all other tasks from the bundle. Waiting for end will not elongate the task itself, only block the respective layer longer.
+        /// <returns></returns>
+        public bool OpenNewBundle(bool waitForStart = true, bool waitForEnd = true)
+        {
+            CloseBundle();
+            if (!waitForStart && !waitForEnd)
+                //The bundle has to wait for start or end, otherwise it doesn't perform any work
+                return false;
+            if (waitForStart)
+                synchroniserStart = new TaskSynchroniser();
+            if (waitForEnd)
+                synchroniserEnd = new TaskSynchroniser();
+            return true;
+        }
+
+        /// <summary>
+        /// Afte closing, tasks can be scheduled normal again.
+        /// </summary>
+        public void CloseBundle()
+        {
+            synchroniserStart = null;
+            synchroniserEnd = null;
+        }
+
+        /// <summary>
+        /// Schedule task and add it to the current bundle, if there is a bundle open currently.
+        /// </summary>
+        /// <param name="task"></param>
+        /// <param name="priortiy"></param>
+        /// <param name="layer"></param> The animation layer on which the task should be excuted
+        public void ScheduleWithRespectTobundle(IAgentTask task, int priortiy = 0, string layer = "Base Layer")
+        {
+            if(synchroniserStart != null)
+            {
+                if (task.ReadyToStart == null)
+                {
+                    task.ReadyToStart = new List<System.Func<bool>>();
+                }
+                task.ReadyToStart.Add(synchroniserStart.WaitForOtherTasksMutually(task));
+            }
+            if (synchroniserEnd != null)
+            {
+                if (task.ReadyToEnd == null)
+                {
+                    task.ReadyToEnd = new List<System.Func<bool>>();
+                }
+                task.ReadyToEnd.Add(synchroniserEnd.WaitForOtherTasksMutually(task));
+            }
+            agent.ScheduleTask(task,priortiy,layer);
         }
     }
 }
