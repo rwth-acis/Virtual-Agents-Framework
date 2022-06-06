5fea5be9fc7b268e23f28ef9b6a9d216c937150b
YanxiLiu <951159548@qq.com>
2022-06-06 12:30:15 +0200

    UI Finished

--- Assets/Virtual Agents Framework/Runtime/Scripts/Speech/RocketChatSpeechClient.cs
+++ Assets/Virtual Agents Framework/Runtime/Scripts/Speech/RocketChatSpeechClient.cs
@@ -8,6 +8,7 @@ using System;
 using Microsoft.MixedReality.Toolkit.UI;
 using Microsoft.MixedReality.Toolkit.Utilities;
 using System.Linq;
+using i5.Toolkit.Core.ServiceCore;
 
 namespace i5.VirtualAgents.Speech
 {
@@ -27,6 +28,8 @@ namespace i5.VirtualAgents.Speech
         private TMP_InputField messageField;
         private GameObject subscribePanel;
         private GameObject channelGroupPanel;
+        private GameObject channelGroupPostPanel;
+        private GameObject channelGroupSubscribePanel;
         private GameObject postAndSubscribePanel;
         private GameObject postMessageToChannelGroupPanel;
 
@@ -38,6 +41,7 @@ namespace i5.VirtualAgents.Speech
         private string messageToWrite;
         //We create a delegate here because Unity API cannot be called outside of the main thread.
         private Action<TMP_InputField> writeMessageAction;
+        private bool roomSubscribed;
         public string HostAddress
         {
             get => loginPanel.transform.Find("HostAddress/unity-text-input/MRKeyboardInputField_TMP(Clone)").gameObject.GetComponent<TMP_InputField>().text;
@@ -59,11 +63,15 @@ namespace i5.VirtualAgents.Speech
             methodPanel = uiPanel.transform.Find("Method Panel").gameObject;
             profilePanel = uiPanel.transform.Find("Profile Panel").gameObject;
             channelGroupPanel = uiPanel.transform.Find("ChannelGroup Panel").gameObject;
+            channelGroupPostPanel = uiPanel.transform.Find("ChannelGroup Post Panel").gameObject;
+            channelGroupSubscribePanel = uiPanel.transform.Find("ChannelGroup Subscribe Panel").gameObject;
             postMessagePanel = uiPanel.transform.Find("PostMessage Panel").gameObject;
             subscribePanel = uiPanel.transform.Find("Subscribe Panel").gameObject;
             agentSpeechProvider = GetComponent<SpeechProvider>();
             postAndSubscribePanel = uiPanel.transform.Find("Post And Subscribe Panel").gameObject;
             postMessageToChannelGroupPanel = uiPanel.transform.Find("Post Message To Channel Group Panel").gameObject;
+            ServiceManager.RegisterService(new RocketChatService(HostAddress));
+            roomSubscribed = false;
         }
 
         // Update is called once per frame
@@ -79,7 +87,7 @@ namespace i5.VirtualAgents.Speech
         /// Login to the host server.
         /// </summary>
         public async void LoginAsync() {
-            client = new RocketChatService(HostAddress);
+            client = ServiceManager.GetService<RocketChatService>();
             bool loginSucceeded = await client.LoginAsync(Username, Password);
             if (loginSucceeded) {
                 SpeakNoticeAsync("Login Succeeded");
@@ -102,6 +110,11 @@ namespace i5.VirtualAgents.Speech
             subscribePanel.SetActive(true);
         }
 
+        public void OnGetJoinedChannelAndGroupSelected() {
+            methodPanel.SetActive(false);
+            channelGroupPanel.SetActive(true);
+        }
+
         public void OnShowMethodsSelected() {
             for(int i = 0; i < uiPanel.transform.childCount; i++) {
                 uiPanel.transform.GetChild(i).gameObject.SetActive(false);
@@ -109,25 +122,29 @@ namespace i5.VirtualAgents.Speech
             uiPanel.transform.Find("Method Panel").gameObject.SetActive(true);
         }
 
-        public async void PostMessageAsync() {
+        public void OnSelectChannelGroupPostSelected() {
+            GetJoinedChannelAndGroupAsync();
+            postMessagePanel.SetActive(false);
+            channelGroupPostPanel.SetActive(true);
+        }
 
-            string target = postMessagePanel.transform.Find("RoomField/unity-text-input/MRKeyboardInputField_TMP(Clone)").gameObject.GetComponent<TMP_InputField>().text;
-            string message = postMessagePanel.transform.Find("MessageField/unity-text-input/MRKeyboardInputField_TMP(Clone)").gameObject.GetComponent<TMP_InputField>().text;
-            var response = await client.PostMessageAsync(target, message);
-            if (response.Successful) {
-                SpeakNoticeAsync("Post Message Succeeded.");
-                postMessagePanel.transform.Find("MessageField/unity-text-input/MRKeyboardInputField_TMP(Clone)").gameObject.GetComponent<TMP_InputField>().text = "";
-            }
-            else {
-                SpeakNoticeAsync($"Post Message Failed. {response.ErrorMessage}");
-            }
-            StopRecordingAsync();
+        public void OnSelectChannelGroupSubscribeSelected() {
+            GetJoinedChannelAndGroupAsync();
+            subscribePanel.SetActive(false);
+            channelGroupSubscribePanel.SetActive(true);
+        }
+
+        public void OnReturnPostAndSubscribeSelected() {
+            postMessageToChannelGroupPanel.SetActive(false);
+            postAndSubscribePanel.SetActive(true);
         }
 
         public async void StartRecordingMessageAsync() {
             agentSpeechProvider.OnRecognitionResultReceived += WriteRecognitionResultToMessageField;
             postMessagePanel.transform.Find("StartRecordingButton").gameObject.SetActive(false);
             postMessagePanel.transform.Find("StopRecordingButton").gameObject.SetActive(true);
+            postMessageToChannelGroupPanel.transform.Find("StartRecordingButton").gameObject.SetActive(false);
+            postMessageToChannelGroupPanel.transform.Find("StopRecordingButton").gameObject.SetActive(true);
             var result = await agentSpeechProvider.StartRecordingAsync();
             switch (result.State) {
                 case ResultState.Succeeded:
@@ -144,22 +161,24 @@ namespace i5.VirtualAgents.Speech
         public async void StopRecordingAsync() {
             agentSpeechProvider.OnRecognitionResultReceived -= WriteRecognitionResultToMessageField;
             messageToWrite = "";
-            await agentSpeechProvider.StopRecordingAsync();
             postMessagePanel.transform.Find("StartRecordingButton").gameObject.SetActive(true);
             postMessagePanel.transform.Find("StopRecordingButton").gameObject.SetActive(false);
-        }
-
-        public async void SubscribeRoomAsync() {
-            string roomID = subscribePanel.transform.Find("RoomField/unity-text-input/MRKeyboardInputField_TMP(Clone)").gameObject.GetComponent<TMP_InputField>().text;
-            await client.SubscribeRoomMessageAsync(roomID, "1");
-            SpeakNoticeAsync("Room Subscribed");
-            client.OnMessageReceived += SpeakMessageAsync;
+            postMessageToChannelGroupPanel.transform.Find("StartRecordingButton").gameObject.SetActive(true);
+            postMessageToChannelGroupPanel.transform.Find("StopRecordingButton").gameObject.SetActive(false);
+            await agentSpeechProvider.StopRecordingAsync();
         }
 
         public async void UnsubscribeRoomAsync() {
-            await client.UnsubscribeRoomMessageAsync("1");
-            SpeakNoticeAsync("Room Unsubscribed");
-            client.OnMessageReceived -= SpeakMessageAsync;
+            if (roomSubscribed) {
+                await client.UnsubscribeRoomMessageAsync("1");
+                SpeakNoticeAsync("Room unsubscribed");
+                client.OnMessageReceived -= SpeakMessageAsync;
+                roomSubscribed = false;
+            }
+            else {
+                SpeakNoticeAsync("No subscribtion");
+            }
+
         }
 
         public async void GetProfileAsync() {
@@ -197,27 +216,39 @@ namespace i5.VirtualAgents.Speech
                     channelGroups = new List<ChannelGroup>(Enumerable.Concat(channelResponse.Content, groupResponse.Content));                  
                     channelGroupButtons = new List<GameObject>();
                     foreach(ChannelGroup channelGroup in channelGroups) {
-                        GameObject button = Instantiate(channelGroupButton);
-                        button.transform.SetParent(channelGroupPanel.transform.Find("Container/GridObjectCollection"), false);
-                        button.name = channelGroup.name;
-                        button.GetComponent<ButtonConfigHelper>().MainLabelText = channelGroup.name;
-                        button.GetComponent<ButtonConfigHelper>().OnClick.AddListener(() => OpenPostAndSubscribePanel(channelGroup));
+                        GameObject button1 = Instantiate(channelGroupButton);
+                        button1.transform.SetParent(channelGroupPanel.transform.Find("Container/GridObjectCollection"), false);
+                        button1.name = channelGroup.name;
+                        button1.GetComponent<ButtonConfigHelper>().MainLabelText = channelGroup.name;
+                        button1.GetComponent<ButtonConfigHelper>().OnClick.AddListener(() => OpenPostAndSubscribePanel(channelGroup));
+
+                        GameObject button2 = Instantiate(channelGroupButton);
+                        button2.transform.SetParent(channelGroupPostPanel.transform.Find("Container/GridObjectCollection"), false);
+                        button2.name = channelGroup.name;
+                        button2.GetComponent<ButtonConfigHelper>().MainLabelText = channelGroup.name;
+                        button2.GetComponent<ButtonConfigHelper>().OnClick.AddListener(() => OpenPostMessagePanel(channelGroup));
+
+                        GameObject button3 = Instantiate(channelGroupButton);
+                        button3.transform.SetParent(channelGroupSubscribePanel.transform.Find("Container/GridObjectCollection"), false);
+                        button3.name = channelGroup.name;
+                        button3.GetComponent<ButtonConfigHelper>().MainLabelText = channelGroup.name;
+                        button3.GetComponent<ButtonConfigHelper>().OnClick.AddListener(() => OpenSubscribePanel(channelGroup));
                     }
                     channelGroupPanel.transform.Find("Container/GridObjectCollection").GetComponent<GridObjectCollection>().UpdateCollection();
-                    methodPanel.SetActive(false);
-                    channelGroupPanel.SetActive(true);
+                    channelGroupPostPanel.transform.Find("Container/GridObjectCollection").GetComponent<GridObjectCollection>().UpdateCollection();
+                    channelGroupSubscribePanel.transform.Find("Container/GridObjectCollection").GetComponent<GridObjectCollection>().UpdateCollection();
+                    channelGroupPanel.GetComponent<ScrollingObjectCollection>().UpdateContent();
+                    channelGroupPostPanel.GetComponent<ScrollingObjectCollection>().UpdateContent();
+                    channelGroupSubscribePanel.GetComponent<ScrollingObjectCollection>().UpdateContent();
                     SpeakNoticeAsync("Get joined channel and group succeeded");
                 }
                 else {
                     SpeakNoticeAsync($"Get joined channel and group failed, {channelResponse.ErrorMessage}");
                 }
             }
-            else {
-                methodPanel.SetActive(false);
-                channelGroupPanel.SetActive(true);
-            }
-
+            //Else, do nothing, since the channels and groups are already retrieved.
         }
+
         #region Private Methods
 
         //Display a message string on the UI.
@@ -246,33 +277,58 @@ namespace i5.VirtualAgents.Speech
         private void OpenPostAndSubscribePanel(ChannelGroup channelGroup) {
             postAndSubscribePanel.SetActive(true);
             channelGroupPanel.SetActive(false);
+            postAndSubscribePanel.transform.Find("PostMessageButton").GetComponent<ButtonConfigHelper>().OnClick.RemoveAllListeners();
+            postAndSubscribePanel.transform.Find("SubscribeButton").GetComponent<ButtonConfigHelper>().OnClick.RemoveAllListeners();
             postAndSubscribePanel.transform.Find("ChannelGroupLabel/Text").GetComponent<TextMeshPro>().text = channelGroup.name;
             postAndSubscribePanel.transform.Find("PostMessageButton").GetComponent<ButtonConfigHelper>().OnClick.AddListener(() => OpenPostMessageToChannelGroupPanel(channelGroup));
             postAndSubscribePanel.transform.Find("SubscribeButton").GetComponent<ButtonConfigHelper>().OnClick.AddListener(() => SubscribeChannelGroupAysnc(channelGroup));
+        }
+
+        private void OpenPostMessagePanel(ChannelGroup channelGroup) {
+            postMessagePanel.SetActive(true);
+            channelGroupPostPanel.SetActive(false);
+            postMessagePanel.transform.Find("PostMessageButton").GetComponent<ButtonConfigHelper>().OnClick.RemoveAllListeners();
+            postMessagePanel.transform.Find("ChannelGroupLabel/Text").GetComponent<TextMeshPro>().text = channelGroup.name;
+            postMessagePanel.transform.Find("PostMessageButton").GetComponent<ButtonConfigHelper>().OnClick.AddListener(() => PostMessageToChannelGroupAsync(channelGroup));
+        }
+
+        private void OpenSubscribePanel(ChannelGroup channelGroup) {
+            subscribePanel.SetActive(true);
+            channelGroupSubscribePanel.SetActive(false);
+            subscribePanel.transform.Find("SubscribeButton").GetComponent<ButtonConfigHelper>().OnClick.RemoveAllListeners();
+            subscribePanel.transform.Find("ChannelGroupLabel/Text").GetComponent<TextMeshPro>().text = channelGroup.name;
+            subscribePanel.transform.Find("SubscribeButton").GetComponent<ButtonConfigHelper>().OnClick.AddListener(() => SubscribeChannelGroupAysnc(channelGroup));
         }
 
         private void OpenPostMessageToChannelGroupPanel(ChannelGroup channelGroup) {
             messageField = postMessageToChannelGroupPanel.transform.Find("MessageField/unity-text-input/MRKeyboardInputField_TMP(Clone)").gameObject.GetComponent<TMP_InputField>();
             postMessageToChannelGroupPanel.SetActive(true);
             postAndSubscribePanel.SetActive(false);
+            postMessageToChannelGroupPanel.transform.Find("PostMessageButton").GetComponent<ButtonConfigHelper>().OnClick.RemoveAllListeners();
             postMessageToChannelGroupPanel.transform.Find("ChannelGroupLabel/Text").GetComponent<TextMeshPro>().text = channelGroup.name;
             postMessageToChannelGroupPanel.transform.Find("PostMessageButton").GetComponent<ButtonConfigHelper>().OnClick.AddListener(() => PostMessageToChannelGroupAsync(channelGroup));
         }
 
         private async void SubscribeChannelGroupAysnc(ChannelGroup channelGroup) {
-            string roomID = channelGroup._id;
-            await client.SubscribeRoomMessageAsync(roomID, "1");
-            SpeakNoticeAsync($"Room {channelGroup.name} Subscribed");
-            client.OnMessageReceived += SpeakMessageAsync;
+            if (roomSubscribed) {
+                SpeakNoticeAsync("Already subscribed one room");
+            }
+            else {
+                roomSubscribed = true;
+                string roomID = channelGroup._id;
+                await client.SubscribeRoomMessageAsync(roomID, "1");
+                SpeakNoticeAsync($"Room {channelGroup.name} Subscribed");
+                client.OnMessageReceived += SpeakMessageAsync;
+            }
         }
 
         private async void PostMessageToChannelGroupAsync(ChannelGroup channelGroup) {
             string target = channelGroup._id;
-            string message = postMessageToChannelGroupPanel.transform.Find("MessageField/unity-text-input/MRKeyboardInputField_TMP(Clone)").gameObject.GetComponent<TMP_InputField>().text;
+            string message = messageField.text;
             var response = await client.PostMessageAsync(target, message);
             if (response.Successful) {
                 SpeakNoticeAsync("Post Message Succeeded.");
-                postMessageToChannelGroupPanel.transform.Find("MessageField/unity-text-input/MRKeyboardInputField_TMP(Clone)").gameObject.GetComponent<TMP_InputField>().text = "";
+                messageField.text = "";
             }
             else {
                 SpeakNoticeAsync($"Post Message Failed. {response.ErrorMessage}");
