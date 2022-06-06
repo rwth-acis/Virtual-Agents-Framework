004424e18cf586c65df9b16052039aaa7b7ec86b
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2022-06-06 15:45:04 +0200

    Trees can now be serialized and saved via the BehaviourTree Asset

---
+++ Runtime/Scripts/BehaviorTree/NodeSerialization/GraphicalNode.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace i5.VirtualAgents
+{
+    /// <summary>
+    /// Allows a node to be used in the graphical beahavior tree editor
+    /// </summary>
+    [Serializable]
+    public class GraphicalNode : TaskSerializer
+    {
+        public GUID guid;
+        public Vector2 position;
+        public string description;
+        public List<GraphicalNode> children = new List<GraphicalNode>();
+
+        public override void OnAfterDeserialize()
+        {
+            base.OnAfterDeserialize();
+            if (serializedTask is ICompositeNode)
+            {
+                foreach (var child in children)
+                {
+                    child.OnAfterDeserialize();
+                    ((ICompositeNode)serializedTask).children.Add((ITask)child.serializedTask);
+                }
+            }
+        }
+    }
+}
