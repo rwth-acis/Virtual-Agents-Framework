2d1220e2f58a59b9b19316e021729789fae8d068
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2022-06-06 15:33:21 +0200

    Moved all Node implementations in there own folder

---
+++ Assets/Virtual Agents Framework/Runtime/Scripts/BehaviorTree/NodeImplementations/SelectorNode.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using i5.VirtualAgents.TaskSystem;
+using System;
+
+namespace i5.VirtualAgents
+{
+    [Serializable]
+    public class SelectorNode : ICompositeNode, ISerializable
+    {
+        public List<ITask> children { get; set; }
+        public TaskState state { get; set; }
+
+        private Agent executingAgent;
+        public SelectorNode()
+        {
+            children = new List<ITask>();
+        }
+
+        
+        public void Execute(Agent executingAgent)
+        {
+            state = TaskState.Running;
+            this.executingAgent = executingAgent;
+        }
+        int current = 0;
+
+        public void Stop()
+        {
+        }
+
+        public TaskState Update()
+        {
+            TaskState currentNodestate = children[current].FullUpdate(executingAgent);
+
+
+            if (currentNodestate == TaskState.Failure)
+            {
+                current++;
+                if (current >= children.Count)
+                    return TaskState.Failure; //All nodes failed, report general failure
+                else
+                    return TaskState.Running; 
+            }
+            else
+            {
+                return currentNodestate;
+            }
+        }
+    }
+}
---
+++ Assets/Virtual Agents Framework/Runtime/Scripts/BehaviorTree/NodeImplementations/SequencerNode.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using i5.VirtualAgents.TaskSystem;
+using System;
+
+namespace i5.VirtualAgents
+{
+    public class SequencerNode : ICompositeNode, ISerializable
+    {
+        //TODO komplizierter getter setter vermutlich nicht nötig
+        List<ITask> _children;
+        public List<ITask> children { get { return _children; } set { _children = value; } }
+        public TaskState state { get; set; }
+        public List<Func<bool>> ReadyToStart { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<Func<bool>> ReadyToEnd { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        int current = 0;
+        Agent executingAgent;
+
+        public SequencerNode()
+        {
+            children = new List<ITask>();
+        }
+
+        public void Execute(Agent executingAgent)
+        {
+            this.executingAgent = executingAgent;
+        }
+
+        public void Stop()
+        {
+        }
+
+        public TaskState Update()
+        {
+            TaskState currentNodestate = children[current].FullUpdate(executingAgent);
+
+            if (currentNodestate == TaskState.Success)
+            {
+                current++;
+                if (current >= children.Count)
+                    return TaskState.Success;
+                else
+                    return TaskState.Running;
+            }
+            else
+            {
+                return currentNodestate;
+            }
+        }
+    }
+}
