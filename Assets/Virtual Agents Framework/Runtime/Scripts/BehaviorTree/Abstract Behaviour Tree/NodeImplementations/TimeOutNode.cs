eb146b2627eb9ffeb0063bac8e472c5ba778f0f1
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2022-08-09 20:38:20 +0200

    Added nodde that lets it child fail, if it doesn't finish in a specified time

---
+++ Assets/Virtual Agents Framework/Runtime/Scripts/BehaviorTree/Abstract Behaviour Tree/NodeImplementations/TimeOutNode.cs
@@ -0,0 +1,56 @@
+using i5.VirtualAgents.TaskSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace i5.VirtualAgents.BehaviourTrees
+{
+    /// <summary>
+    /// If its child doesn't finish in timeOutInSeconds, it fails
+    /// </summary>
+    public class TimeOutNode : DecoratorNode, ISerializable
+    {
+        private float timeOutInSeconds;
+        private Coroutine timeOutRoutine;
+
+
+        public override TaskState Update()
+        {
+            TaskState state = Child.FullUpdate(executingAgent);
+            if (state == TaskState.Success || state == TaskState.Failure)
+            {
+                // Task finished faster than the timeout time, stop routine to prevent the task from being stopped multiple times
+                executingAgent.StopCoroutine(timeOutRoutine);
+            }
+            return state;
+        }
+
+        public override void Execute(Agent executingAgent)
+        {
+            base.Execute(executingAgent);
+            if (timeOutRoutine != null)
+            {
+                executingAgent.StopCoroutine(timeOutRoutine);
+            }
+            timeOutRoutine = executingAgent.StartCoroutine(TimeOut(timeOutInSeconds));
+        }
+
+        // Wait for the given time and then fail the task
+        private IEnumerator TimeOut(float timeInSeconds)
+        {
+            yield return new WaitForSeconds(timeInSeconds);
+            PreemptivelyFailTask();
+        }
+
+        public void Deserialize(SerializationDataContainer serializer)
+        {
+            timeOutInSeconds = serializer.GetSerializedFloat("Time Out in Seconds");
+        }
+
+        public void Serialize(SerializationDataContainer serializer)
+        {
+            serializer.AddSerializedData("Time Out in Seconds", timeOutInSeconds);
+        }
+    }
+}
