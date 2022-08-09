89406445eb55b7102e15ef069488ef7d63239a09
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2022-08-09 20:25:53 +0200

    Added node that executes one of its child tasks at random

---
+++ Assets/Virtual Agents Framework/Runtime/Scripts/BehaviorTree/Abstract Behaviour Tree/NodeImplementations/RandomNode.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using i5.VirtualAgents.TaskSystem;
+using System;
+
+namespace i5.VirtualAgents.BehaviourTrees
+{
+    /// <summary>
+    /// Executes one of its child tasks at random
+    /// </summary>
+    public class RandomNode : CompositeNode, ISerializable
+    {
+        private ITask randomTask;
+
+        public override void Execute(Agent executingAgent)
+        {
+            base.Execute(executingAgent);
+            UnityEngine.Random.InitState(DateTime.Now.Millisecond);
+            randomTask = Children[UnityEngine.Random.Range(0, Children.Count)];
+        }
+
+        public override TaskState Update()
+        {
+            return randomTask.FullUpdate(executingAgent);
+        }
+
+        public void Deserialize(SerializationDataContainer serializer)
+        {
+        }
+
+        public void Serialize(SerializationDataContainer serializer)
+        {
+        }
+    }
+}
