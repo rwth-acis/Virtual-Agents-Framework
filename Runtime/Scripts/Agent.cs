4137fab80952cbb16a544e3b5b287dd2f09092f4
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2022-08-21 14:27:50 +0200

    Agent not throws a warning when no TaskSytem is attached

--- Runtime/Scripts/Agent.cs
+++ Runtime/Scripts/Agent.cs
@@ -24,7 +24,14 @@ namespace i5.VirtualAgents
         private void Awake()
         {
             Animator = GetComponent<Animator>();
-            TaskSystem = GetComponent<TaskSystem.TaskSystem>();
+            TaskSystem = GetComponent<ScheduleBasedExecution.TaskSystem>();
+            // Since there are multiple TaskSystems, enforcing one with RequireComponent is not advisable.
+            // If the parent class TaskSystem is enforced, an agent will automatically get a component that implemenets no own functionallity, that can not easily be deleted and that can be confused with the actual task system.
+            // If one of its implementations is enforced, it is harder to use one of the other implemetations.
+            if (TaskSystem == null)
+            {
+                Debug.LogWarning("Agent has no TaskSystem attached. Attach a component that inherits the TaskSystem class.");
+            }
         }
 
         /// <summary>
