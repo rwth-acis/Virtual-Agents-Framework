85f86c138ec4f141da889e5b0f3637cfb346344a
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2022-09-01 16:57:06 +0200

    Extracted CreatedNodesOverwriteData into a function

--- Editor/Scripts/Behaviour Tree/BehaviourTreeRunnerInspector.cs
+++ Editor/Scripts/Behaviour Tree/BehaviourTreeRunnerInspector.cs
@@ -74,72 +74,7 @@ namespace i5.VirtualAgents.Editor
 
             if (nodeOverwriteData == null)
             {
-                // No data found => create it!
-                int size = nodesData.arraySize;
-                nodesData.InsertArrayElementAtIndex(size); // Insert a new entry at the end
-                var entry = nodesData.GetArrayElementAtIndex(size);
-                entry.FindPropertyRelative("Key").stringValue = view.node.Guid;
-                nodeOverwriteData = entry.FindPropertyRelative("Value");
-
-                // Copys all data from the seralizationData List origin into the serialized seralizationData array destination
-                void CopySerializedData <T> (List<SerializationEntry<T>> origin, string destinationPath)
-                {
-                    SerializedProperty destination = nodeOverwriteData.FindPropertyRelative(destinationPath);
-                    for (int i = 0;  i < origin.Count; i++)
-                    {
-                        SerializationEntry<T> data = origin[i];
-                        destination.InsertArrayElementAtIndex(i); //Make space for the new data entry in the serialized array
-                        SerializedProperty arrayElement = destination.GetArrayElementAtIndex(i);
-
-                        // Copy key and data
-                        arrayElement.FindPropertyRelative("Key").stringValue = data.Key;
-                        SerializedProperty value = arrayElement.FindPropertyRelative("Value");
-                        if (typeof(T) == typeof(Vector3))
-                        {
-                            value.vector3Value = (Vector3)(data.Value as Vector3?); // This is neccesarry, since direct cast can't be used because T is not constrained to inherit from Vector3 and the as operator
-                                                                                    // can only be used on nullable types. Therfore the conversion to the nullable type Vector3? which is then casted to the actual Vector3 type
-                        }
-                        else if (typeof(T) == typeof(float))
-                        {
-                            value.floatValue = (float)(data.Value as float?);
-                        }
-                        else if (typeof(T) == typeof(string))
-                        {
-                            value.stringValue = data.Value as string;
-                        }
-                        else if (typeof(T) == typeof(int))
-                        {
-                            value.intValue = (int)(data.Value as int?);
-                        }
-                        else if (typeof(T) == typeof(GameObject))
-                        {
-                            value.objectReferenceValue = data.Value as GameObject;
-                        }
-                        else if (typeof(T) == typeof(bool))
-                        {
-                            value.boolValue = (bool)(data.Value as bool?);
-                        }
-                        else if (typeof(T) == typeof(List<float>))
-                        {
-                            List<float> floatList = data.Value as List<float>;
-                            for (int j = 0; j < floatList.Count; j++)
-                            {
-                                value.InsertArrayElementAtIndex(j);
-                                value.GetArrayElementAtIndex(j).floatValue = floatList[j];
-                            }
-                        }
-                        
-                    }
-                }
-
-                // Copy the serialization data from the node to the newly created nodesData
-                CopySerializedData(view.node.Data.serializedVectors.data, "serializedVectors.data");
-                CopySerializedData(view.node.Data.serializedFloats.data, "serializedFloats.data");
-                CopySerializedData(view.node.Data.serializedStrings.data, "serializedStrings.data");
-                CopySerializedData(view.node.Data.serializedInts.data, "serializedInts.data");
-                CopySerializedData(view.node.Data.serializedGameobjects.data, "serializedGameobjects.data");
-                CopySerializedData(view.node.Data.serializedBools.data, "serializedBools.data");
-                CopySerializedData(view.node.Data.serializedListFloats.data, "serializedListFloats.data");
+                nodeOverwriteData = CreateNodeOverwriteData(nodesData, view);
             }
             // Create Property fields for the overwrite data
 
@@ -161,6 +96,7 @@ namespace i5.VirtualAgents.Editor
             int gameobjectCounter = 0;
             int boolCounter = 0;
             int listFloatCounter = 0;
+            int treesCounter = 0;
 
             SerializableType[] serializationOrder = new SerializableType[targetNode.Data.serializationOrder.Count];
 
@@ -192,12 +128,97 @@ namespace i5.VirtualAgents.Editor
                     case SerializableType.LIST_FLOAT:
                         CreatePropertyField("serializedListFloats", ref listFloatCounter, SerializableType.LIST_FLOAT, targetNode, nodeOverwriteData);
                         break;
+                    case SerializableType.TREE:
+                        CreatePropertyField("serializedTrees", ref treesCounter, SerializableType.TREE, targetNode, nodeOverwriteData);
+                        break;
                     default:
-                        throw new NotImplementedException();
+                        throw new NotImplementedException(type + " has no property field handler");
                 }
             }
 
             serializedObject.ApplyModifiedProperties();
+        }
+
+
+        private SerializedProperty CreateNodeOverwriteData(SerializedProperty nodesData, NodeView view)
+        {
+            // No data found => create it!
+            int size = nodesData.arraySize;
+            nodesData.InsertArrayElementAtIndex(size); // Insert a new entry at the end
+            var entry = nodesData.GetArrayElementAtIndex(size);
+            entry.FindPropertyRelative("Key").stringValue = view.node.Guid;
+            SerializedProperty nodeOverwriteData = entry.FindPropertyRelative("Value");
+
+            // Copys all data from the seralizationData List origin into the serialized seralizationData array destination
+            void CopySerializedData<T>(List<SerializationEntry<T>> origin, string destinationPath)
+            {
+                SerializedProperty destination = nodeOverwriteData.FindPropertyRelative(destinationPath);
+                for (int i = 0; i < origin.Count; i++)
+                {
+                    SerializationEntry<T> data = origin[i];
+                    destination.InsertArrayElementAtIndex(i); //Make space for the new data entry in the serialized array
+                    SerializedProperty arrayElement = destination.GetArrayElementAtIndex(i);
+
+                    // Copy key and data
+                    arrayElement.FindPropertyRelative("Key").stringValue = data.Key;
+                    SerializedProperty value = arrayElement.FindPropertyRelative("Value");
+                    if (typeof(T) == typeof(Vector3))
+                    {
+                        value.vector3Value = (Vector3)(data.Value as Vector3?); // This is neccesarry, since direct cast can't be used because T is not constrained to inherit from Vector3 and the as operator
+                                                                                // can only be used on nullable types. Therfore the conversion to the nullable type Vector3? which is then casted to the actual Vector3 type
+                    }
+                    else if (typeof(T) == typeof(float))
+                    {
+                        value.floatValue = (float)(data.Value as float?);
+                    }
+                    else if (typeof(T) == typeof(string))
+                    {
+                        value.stringValue = data.Value as string;
+                    }
+                    else if (typeof(T) == typeof(int))
+                    {
+                        value.intValue = (int)(data.Value as int?);
+                    }
+                    else if (typeof(T) == typeof(GameObject))
+                    {
+                        value.objectReferenceValue = data.Value as GameObject;
+                    }
+                    else if (typeof(T) == typeof(bool))
+                    {
+                        value.boolValue = (bool)(data.Value as bool?);
+                    }
+                    else if (typeof(T) == typeof(List<float>))
+                    {
+                        List<float> floatList = data.Value as List<float>;
+                        for (int j = 0; j < floatList.Count; j++)
+                        {
+                            value.InsertArrayElementAtIndex(j);
+                            value.GetArrayElementAtIndex(j).floatValue = floatList[j];
+                        }
+                    }
+                    else if (typeof(T) == typeof(BehaviorTreeAsset))
+                    {
+                        value.objectReferenceValue = data.Value as BehaviorTreeAsset;
+                    }
+                    else
+                    {
+                        throw new NotImplementedException(typeof(T) + " has no copy handler");
+                    }
+
+                }
+            }
+
+            // Copy the serialization data from the node to the newly created nodesData
+            CopySerializedData(view.node.Data.serializedVectors.data, "serializedVectors.data");
+            CopySerializedData(view.node.Data.serializedFloats.data, "serializedFloats.data");
+            CopySerializedData(view.node.Data.serializedStrings.data, "serializedStrings.data");
+            CopySerializedData(view.node.Data.serializedInts.data, "serializedInts.data");
+            CopySerializedData(view.node.Data.serializedGameobjects.data, "serializedGameobjects.data");
+            CopySerializedData(view.node.Data.serializedBools.data, "serializedBools.data");
+            CopySerializedData(view.node.Data.serializedListFloats.data, "serializedListFloats.data");
+            CopySerializedData(view.node.Data.serializedTrees.data, "serializedTrees.data");
+
+            return nodeOverwriteData;
         }
 
         // Creates a property field of the provided type for the serized data saved in the array with the name propertyName
