47f1199efaff4e96b6e490a377b3faba9d9aa2e5
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2022-10-15 15:26:05 +0200

    removed child classes of agent so that developers just need to switch the task system and not the agent component

--- Assets/Virtual Agents Framework/Runtime/Scripts/TaskSystem.cs
+++ Assets/Virtual Agents Framework/Runtime/Scripts/TaskSystem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace i5.VirtualAgents.ScheduleBasedExecution
+namespace i5.VirtualAgents
 {
     public abstract class TaskSystem : MonoBehaviour, ITaskSystem
     {
