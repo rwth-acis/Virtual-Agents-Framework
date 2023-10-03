e9f82c3ab3545520902936170a324e3ef6c29344
Davidterhuerne <davidterhuerne@web.de>
2023-10-03 23:51:26 +0200

    added ability to drop items

---
+++ Assets/Virtual Agents Framework/Runtime/Scripts/FundamentalAgentTasks/AgentDropTask.cs
@@ -0,0 +1,101 @@
+using i5.Toolkit.Core.Utilities;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Animations.Rigging;
+using System.Collections;
+using System.Collections.Generic;
+using static Codice.Client.Common.WebApi.WebApiEndpoints;
+
+namespace i5.VirtualAgents.AgentTasks
+{
+    /// <summary>
+    /// Defines pick up tasks for picking up objects that are near to the agent
+    /// Uses the NavMeshAgent component
+    /// </summary>
+    public class AgentDropTask : AgentBaseTask, ISerializable
+    {
+
+
+        /// <summary>
+        /// Object that should be picked up
+        /// </summary>
+        public GameObject DropObject { get; protected set; }
+
+        public AgentDropTask()
+        {
+        }
+
+        /// <summary>
+        /// Create an AgentDropTask using the object that is currently carried and should be dropped, if no object is given, the agent will drop all objects that are currently carried
+        /// </summary>
+        /// <param name="dropObject">The object that the agent should drop</param>
+        public AgentDropTask(GameObject dropObject = null)
+        {
+            DropObject = dropObject;
+        }
+
+        /// <summary>
+        /// Starts the drop task
+        /// </summary>
+        /// <param name="agent">The agent which should execute the drop task</param>
+        public override void StartExecution(Agent agent)
+        {
+            base.StartExecution(agent);
+            if (DropObject == null)
+            {
+                dropAllItems(agent, agent.transform);
+                Debug.Log("Objects were droped");
+            }
+            else
+            {
+                Item item = DropObject.GetComponent<Item>();
+                if (item == null)
+                {
+                    State = TaskState.Failure;
+                    i5Debug.LogError($"The drop object {DropObject.name} does not have a Item component. " +
+                        $"Therefore, it cannot be dropped. Skipping this task.",
+                        this);
+                }
+                dropOneItem(agent, item);
+                Debug.Log("Object was droped");
+            }
+
+            FinishTask();
+        }
+
+
+        public void dropAllItems(Agent agent, Transform currentTransform)
+        {
+            // Check if the current transform has the "ItemComponent"
+            Item item = currentTransform.GetComponent<Item>();
+
+            if (item != null)
+            {
+                dropOneItem(agent, item);
+            }
+
+            // Recursively check each child
+            foreach (Transform child in currentTransform)
+            {
+                dropAllItems(agent, child);
+            }
+        }
+        public void dropOneItem(Agent agent, Item item)
+        {
+            item.transform.SetParent(null, true);
+            item.isPickedUp = false;
+        }
+
+        public void Serialize(SerializationDataContainer serializer)
+        {
+            serializer.AddSerializedData("Drop Object", DropObject);
+        }
+
+        public void Deserialize(SerializationDataContainer serializer)
+        {
+            DropObject = serializer.GetSerializedGameobjects("Drop Object");
+        }
+        
+
+    }
+}
--- Assets/Virtual Agents Framework/Samples/ItemPickUpSample/ItemPickUpSampleController.cs
+++ Assets/Virtual Agents Framework/Samples/ItemPickUpSample/ItemPickUpSampleController.cs
@@ -18,6 +18,15 @@ namespace i5.VirtualAgents.Examples
             {
                 taskSystem.Tasks.GoToAndPickUp(waypoints[i],default);
             }
+
+            // add a task to go to the start an drop the first item
+            taskSystem.Tasks.GoToAndDropItem(Vector3.zero,waypoints[0]);
+
+            // add a task to move a bit and then drop all item
+            taskSystem.Tasks.GoToAndDropItem(new Vector3(10,0,0));
+
+            //move away from the items
+            taskSystem.Tasks.GoTo(new Vector3(5,0,0));
         }
     }
 }
