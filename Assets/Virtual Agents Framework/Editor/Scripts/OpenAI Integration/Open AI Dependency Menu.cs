42e65a735f0ae3c1e29b7d8fd033241c555f4e20
Davidterhuerne <davidterhuerne@web.de>
2024-01-22 16:56:40 +0100

    Rename

--- Assets/Virtual Agents Framework/Editor/Scripts/OpenAI Integration/Open AI Dependency Menu.cs
+++ Assets/Virtual Agents Framework/Editor/Scripts/OpenAI Integration/Open AI Dependency Menu.cs
@@ -11,15 +11,17 @@ namespace i5.VirtualAgents.Editor
         [MenuItem("Virtual Agents Framework/OpenAI/Add unofficial OpenAI Helper Package")]
         static void AddDependencies()
         {
-            Client.Add("https://github.com/srcnalt/OpenAI-Unity.git");
             Debug.Log("Added unofficial OpenAI Helper Package to the package menager. Download will automaticlly start in a few seconds...");
+            Client.Add("https://github.com/srcnalt/OpenAI-Unity.git");
+            
         }
 
         [MenuItem("Virtual Agents Framework/OpenAI/Remove OpenAI Helper Package")]
         static void RemoveDependencies()
         {
-            Client.Remove("com.srcnalt.openai-unity");
             Debug.Log("Removed unofficial OpenAI Helper Package from the package menager.");
+            Client.Remove("com.srcnalt.openai-unity");
+            
         }
         [MenuItem("Virtual Agents Framework/OpenAI/Link to OpenAI Helper Package")]
         static void Link()
