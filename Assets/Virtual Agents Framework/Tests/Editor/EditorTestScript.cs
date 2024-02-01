9d9c4239a22d69d61be2116b4e993d1024d1a0bd
Davidterhuerne <davidterhuerne@web.de>
2024-02-02 00:15:48 +0100

    Added basic unit tests that run all sample scenes

---
+++ Assets/Virtual Agents Framework/Tests/Editor/EditorTestScript.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace i5.VirtualAgents.Editor.Tests
+{
+    public class EditorTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void EditorTestMenuItems()
+        {
+            // Use the Assert class to test conditions
+        }
+    }
+}
