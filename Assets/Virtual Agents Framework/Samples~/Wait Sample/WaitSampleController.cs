f7afab65efae95af0823db06cbea579ae39dbf84
Davidterhuerne <davidterhuerne@web.de>
2024-03-27 17:56:43 +0100

    rename test

---
+++ Assets/Virtual Agents Framework/Samples~/Wait Sample/WaitSampleController.cs
@@ -0,0 +1,19 @@
+using i5.VirtualAgents;
+using UnityEngine;
+using i5.VirtualAgents.ScheduleBasedExecution;
+
+namespace i5.VirtualAgents.Examples
+{
+    public class WaitSampleController : SampleScheduleController
+    {
+        public Transform[] waypoints;
+
+        protected override void Start()
+        {
+            base.Start();
+            taskSystem.Tasks.GoTo(waypoints[0]);
+            taskSystem.Tasks.WaitForSeconds(2f);
+            taskSystem.Tasks.GoTo(waypoints[1]);
+        }
+    }
+}
