b8734f17b4fcb9a6cab2d8f8c6019a1cebf19ba6
Davidterhuerne <davidterhuerne@web.de>
2024-03-28 20:50:50 +0100

    Test if Packaging for release works
    Test if unpacking works correctly
    
    Test 2
    
    Added user dialog, added autosave for scripting define, only allow correct method

--- Assets/Automation/PackageExporter.cs
+++ Assets/Automation/PackageExporter.cs
@@ -8,18 +8,28 @@ using UnityEditor.Build;
 namespace InternalTools
 {
     /// <summary>
-    /// This class is used to package the Virtual Agents Framework for release.
+    /// This class is used to prepare the Virtual Agents Framework for release.
     /// These methods will not be displayed when the package is used by other projects, as they are not in the Assets folder.
     /// It uses the C# class System.IO to rename the Samples folder to Samples~ and back.
     /// The SAMPLES_PACKAGED define is set and removed to enable/disable the tests that run on the samples.
+    /// Based on the define, only the right method (Release/Development) will be displayed in the Unity Editor.
     /// </summary>
     public class PackageExporter : EditorWindow
     {
         static string path = "Assets/Virtual Agents Framework";
 
-        [MenuItem("Virtual Agents Framework/Package for release")]
+#if !SAMPLES_PACKAGED
+        [MenuItem("Virtual Agents Framework/Prepare for release")]
         static void RenameForRelease()
         {
+            bool confirm = EditorUtility.DisplayDialog("Prepare For Release: Save All Changes?", "In the process of preparing the package for release all changes will be saved. Are you sure you want to do that?", "Save All", "Do Not Save");
+            if (!confirm)
+            {
+                UnityEngine.Debug.Log("Preparation for release cancelled.");
+                return;
+            }
+            // Save all changes so that potential saves in the samples are not lost
+            AssetDatabase.SaveAssets();
             UnityEngine.Debug.Log("1. Renaming \"Samples\" to \"Samples~\"");
             Directory.Move(path + "/Samples", path + "/Samples~");
             UnityEngine.Debug.Log("2. Deleting old Samples.meta");
@@ -28,35 +38,45 @@ namespace InternalTools
             UnityEngine.Debug.Log("3. Setting \"SAMPLES_PACKAGED\" define.");
             SetScriptDefine("SAMPLES_PACKAGED");
             AssetDatabase.Refresh();
-            UnityEngine.Debug.Log("Packaging finished. Unity should recompile soon...");
+            UnityEngine.Debug.Log("Preparation for release finished. Unity should start to recompile soon...");
         }
-        [MenuItem("Virtual Agents Framework/Unpackage for development")]
+#endif
+#if SAMPLES_PACKAGED
+        [MenuItem("Virtual Agents Framework/Prepare for development")]
         static void RenameForDevelopment()
         {
+            bool confirm = EditorUtility.DisplayDialog("Prepare For Development: Save All Changes?", "In the process of preparing the package for development all changes will be saved. Are you sure you want to do that?", "Save All", "Do Not Save");
+            if (!confirm)
+            {
+                UnityEngine.Debug.Log("Preparation for release cancelled.");
+                return;
+            }
             UnityEngine.Debug.Log("1. Renaming \"Samples~\" to \"Samples\"");
             Directory.Move(path + "/Samples~", path + "/Samples");
             // Unity will automatically create a new Samples.meta file, so we don't need to do that.
             // Samples~ doens't have a Samples.meta file, so we don't need to delete it.
             UnityEngine.Debug.Log("2. Removing \"SAMPLES_PACKAGED\" define.");
-            RemoveScriptDefine("SAMPLES_PACKAGED");
+            RemoveScriptDefine("SAMPLES_PACKAGED"); // This will also save all assets to save the change in the project settings
             AssetDatabase.Refresh();
-            UnityEngine.Debug.Log("Unpacking finished. Unity should recompile soon...");
+            UnityEngine.Debug.Log("Preperation for development finished. Unity should start to recompile soon...");
         }
+#endif
 
         static void SetScriptDefine(string addDefine)
         {
             string[] defines;
             PlayerSettings.GetScriptingDefineSymbols(NamedBuildTarget.Standalone, out defines);
             List<string> definesList = defines.ToList();
-            //If the define is already set, remove all occurences of it and add it again
+            // If the define is already set, remove all occurences of it and add it again
             definesList.RemoveAll(o => o == addDefine);
             //Add new define
             definesList.Add(addDefine);
 
             PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.Standalone, definesList.ToArray());
-            //Check that everything went correctly
+            // Check that everything went correctly
             PlayerSettings.GetScriptingDefineSymbols(NamedBuildTarget.Standalone, out defines);
             DebugArray(defines);
+            AssetDatabase.SaveAssets();
         }
 
         static void RemoveScriptDefine(string removeDefine)
@@ -66,11 +86,12 @@ namespace InternalTools
             defines = defines.Where(o => o != removeDefine).ToArray();
             DebugArray(defines);
             PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.Standalone, defines);
+            AssetDatabase.SaveAssets();
         }
 
         static void DebugArray(string[] array)
         {
-            string s = "Defines: ";
+            string s = "Defines:  ";
             foreach (string d in array)
             {
                 s = s + d + ", ";
