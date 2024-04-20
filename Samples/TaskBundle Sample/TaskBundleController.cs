1930c6ad42abe97502ec55cd0a49a3caf1902c1a
Lena Linke <130255332+lenalinke@users.noreply.github.com>
2024-04-20 18:41:54 +0200

    Make adjustments to TaskBundles to ensure that tasks are run after another

--- Samples/TaskBundle Sample/TaskBundleController.cs
+++ Samples/TaskBundle Sample/TaskBundleController.cs
@@ -12,27 +12,42 @@ namespace i5.VirtualAgents.Examples
         protected override void Start()
         {
             base.Start();
+
+            taskSystem.ScheduleTask(new AgentMovementTask(waypoints[0].position ),1);
+            Debug.Log("Running TaskBundleController");
             //Create a list of tasks
-            List<IAgentTask> tasks = new List<IAgentTask>();
+            List<AgentBaseTask> tasks = new List<AgentBaseTask>();
 
             //Create a list of preconditions
-            List<bool> preconditions = new List<bool>();
-            Vector3 agentPosition = agent.transform.position;
-            //preconditions.Add(waypoints[0].position != agentPosition);
+            List<System.Func<bool>> preconditions = new List<System.Func<bool>>();
+            //preconditions.Add(true);
+            //preconditions.Add(waypoints[0].position != agent.transform.position);
+            preconditions.Add(() =>
+            {
+                Debug.Log("precondition 1");
+                //Compare vectors equality with accuracy of 0.5
 
-        //Add Tasks to the tasks List
-            tasks.Add(new AgentAnimationTask("WaveRight", 5));
+                float distance = Vector3.Distance(waypoints[0].position, agent.transform.position);
+                Debug.Log("Distance: " + distance);
+                Debug.Log(distance > 1.0f);
+                return distance <= 1.0f;
+            });
+
+            //Add Tasks to the tasks List
+            //tasks.Add(new AgentAnimationTask("WaveRight", 5));
             for (int i = 0; i < waypoints.Count; i++)
             {
                 tasks.Add(new AgentMovementTask(waypoints[i].position));
             }
 
             //Create a new TaskBundle
-            TaskBundle taskBundle = new TaskBundle(tasks, preconditions);
+            TaskBundle taskBundle = new TaskBundle(this, tasks, preconditions);
 
             // Schedule the TaskBundle to the taskSystem
             //taskSystem.Tasks.GoTo(waypoints[0].position);
-            taskSystem.ScheduleTask(taskBundle);
+            Debug.Log("before ScheduleTask");
+            taskSystem.ScheduleTask(taskBundle, 0);
+            Debug.Log("after ScheduleTask");
 
         }
     }
