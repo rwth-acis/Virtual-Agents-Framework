876a559074ee408d10abff42a0cc82140325c739
Davidterhuerne <davidterhuerne@web.de>
2026-06-16 23:35:11 +0200

    Added GoToAndSit Taskaction, updated sample scene and documentation, added xrefs to documentation

--- Assets/Virtual Agents Framework/Samples/Sitting Sample/AgentSittingController.cs
+++ Assets/Virtual Agents Framework/Samples/Sitting Sample/AgentSittingController.cs
@@ -6,16 +6,23 @@ namespace i5.VirtualAgents.Examples
 {
     public class AgentSittingController : SampleScheduleController
     {
-        [SerializeField] GameObject Chair = null;
-        [SerializeField] GameObject Stool = null;
+        [SerializeField] Chair Chair = null;
+        [SerializeField] Chair Stool = null;
+        [SerializeField] GameObject Item = null;
+        
+        
         protected override void Start()
         {
             base.Start();
             
             taskSystem.Tasks.GoToAndSit(Chair, SittingDirection.SITDOWN);
             taskSystem.Tasks.WaitForSeconds(3);
+            // GoToAndSit is also used for making the agent standup from a chair
             taskSystem.Tasks.GoToAndSit(Chair, SittingDirection.STANDUP);
             
+            taskSystem.Tasks.GoToAndPickUp(Item);
+            
+            // If no SittingDirection is defined it will automatically be toggled
             taskSystem.Tasks.GoToAndSit(Stool);
             taskSystem.Tasks.WaitForSeconds(3);
             taskSystem.Tasks.GoToAndSit(Stool);
